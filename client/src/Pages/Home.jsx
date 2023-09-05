770c09c10f58771554b525c0e6369aa77cf270d2
muhia <109690494+Xmuhia@users.noreply.github.com>
2023-09-05 23:56:05 +0300

    Create Home.jsx

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,18 @@
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../config/motion'
+
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home;
