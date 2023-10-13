71669d0ef399696b6bf37137323a96641ab240b6
muhia <109690494+Xmuhia@users.noreply.github.com>
2023-10-14 00:10:21 +0300

    Update Home.jsx

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -15,7 +15,35 @@ const Home = () => {
 
   return (
     <AnimatePresence>
+      {snap.intro && (
+          <motion.section className="home" {...slideAnimation ('left')}>
+            <motion.header {...slideAnimation('down')}>
+              <img
+                  src='./threejs.png'
+                  alt='logo'
+                  className='w-8 h-8 object-contain'
+              />
+            </motion.header>
 
+            <motion.div className='home-content' {...headContainerAnimation}>
+              
+              <motion.div>
+                <h1 className='head-text'>
+                  MAKE <br className='xl:block hidden' /> IT HAPPEN.
+                </h1>
+              </motion.div>
+
+              <motion.div>
+                <p className='max-w-md font-normal text-gray-600 text-base'>
+                Design your own one-of-a-kind shirt with our revolutionary 3D customization tool. 
+                <strong> Let your creativity run wild</strong> and express your unique style.
+                </p>
+              </motion.div>
+
+            </motion.div>
+
+              </motion.section>
+)}
     </AnimatePresence>
   )
 }
