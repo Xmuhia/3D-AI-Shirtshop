2b26c95d8b779f6ba55741c937b2ddb7ec3ae01f
muhia <109690494+Xmuhia@users.noreply.github.com>
2023-10-15 00:02:32 +0300

    Adding more Animations & Custom Button

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import { motion, AnimatePresence } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 import state from '../store';
+import { CustomButton } from '../components';
 
 import {
   headContainerAnimation,
@@ -33,11 +34,22 @@ const Home = () => {
                 </h1>
               </motion.div>
 
-              <motion.div>
+              <motion.div
+                {...headContentAnimation}
+                className='flex flex-col gap-5'
+              >
                 <p className='max-w-md font-normal text-gray-600 text-base'>
                 Design your own one-of-a-kind shirt with our revolutionary 3D customization tool. 
-                <strong> Let your creativity run wild</strong> and express your unique style.
+                <strong><i> Let your creativity run wild </i></strong> and express your unique style.
                 </p>
+
+                  <CustomButton
+                    type='filled'
+                    title='Customize It'
+                    handleClick={() => state.intro = false}
+                    customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+                  />
+
               </motion.div>
 
             </motion.div>
