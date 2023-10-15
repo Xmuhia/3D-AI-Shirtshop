62363a32caa36db8eb6e92a2e822379ac9560a37
muhia <109690494+Xmuhia@users.noreply.github.com>
2023-10-15 23:59:47 +0300

    Updating State and Adding New Features

--- client/src/Components/CustomButton.jsx
+++ client/src/Components/CustomButton.jsx
@@ -1,10 +1,24 @@
- import React from 'react'
+import React from 'react'
+import state from '../store';
+import { snapshot, useSnapshot } from 'valtio';
 
-const CustomButton = () => {
+const CustomButton = ({type, title, customStyles, handleClick}) => {
+    const snap = useSnapshot(state);
+
+    const generateStyle = (type) => {
+     if(type === 'filled') {
+        return{
+            backgroundColor: snap.color,
+            color: '#fff'
+        }
+     }
+    }
   return (
-    <button className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
-            style={generate(type)}
+    <button
+        className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+        style={generateStyle(type)}
     >
+        {title}
     </button>
   )
 }
