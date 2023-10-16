9ec1260d75254e6abfff1512475ea3ac5f9bc73e
muhia <109690494+Xmuhia@users.noreply.github.com>
2023-10-16 23:58:03 +0300

    Update Customizer.jsx

--- client/src/Pages/Customizer.jsx
+++ client/src/Pages/Customizer.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import config from '../config/config';
+import state from '../store';
+import {download} from '../assets';
+
 
 const Customizer = () => {
   return (
