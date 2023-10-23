645e71ab0d7d812b1c61964cae4100ee96485bcb
muhia <109690494+Xmuhia@users.noreply.github.com>
2023-10-23 23:54:13 +0300

    Updating Editor Tabs and Custom Buttom

--- client/src/Pages/Customizer.jsx
+++ client/src/Pages/Customizer.jsx
@@ -4,12 +4,55 @@ import { useSnapshot } from 'valtio';
 
 import config from '../config/config';
 import state from '../store';
-import {download} from '../assets';
+import { download } from '../assets';
+import { downloadCanvasToImage, reader } from '../config/helpers';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
+import { fadeAnimation, slideAnimation } from '../config/motion';
 
+import { AIPicker, Tab, CustomButton,ColorPicker,FilePicker } from '../components';
 
 const Customizer = () => {
+  const snap = useSnapshot(state);
+
   return (
-    <div>Customizer</div>
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+        <motion.div
+          key='custom'
+          className='absolute top-o left-0 z-10'
+          {...slideAnimation('left')}
+        >
+          <div className='flex items-center min-h-screen'>
+            <div>
+              <div className='editortabs-container tabs'>
+                {EditorTabs.map((tab) => (
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => {}}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+
+        </motion.div>
+        <motion.div 
+          className='absolute z-10 top-5 right-5'
+          {...fadeAnimation}
+        >
+          <CustomButton
+            type='filled'
+            title='Go Back'
+            handleClick={() => state.intro = true}
+            customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+          />
+
+        </motion.div>
+        </>
+      )}
+    </AnimatePresence>
   )
 }
 
