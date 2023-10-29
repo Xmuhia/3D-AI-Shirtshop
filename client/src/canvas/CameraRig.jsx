79b9708a347e99c0da743cbe7357ddc40fd0741f
muhia <109690494+Xmuhia@users.noreply.github.com>
2023-10-29 23:20:15 +0300

    Updating the model rotation

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,4 +1,4 @@
-import React, { Children } from 'react';
+import React, { Children, useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { easing } from 'maath';
 import { useSnapshot } from 'valtio';
@@ -7,6 +7,22 @@ import state from '../store';
 
 
 const CameraRig = ({children}) => {
+  const group = useRef();
+  const snap = useSnapshot(state);
+
+  useFrame((state, delta) => {
+
+  //set the model rotation smoothly
+    easing.dampE(
+      group.current.rotation, [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    )
+  })
+
+
+
+
   return (
     <group>
       {children}
