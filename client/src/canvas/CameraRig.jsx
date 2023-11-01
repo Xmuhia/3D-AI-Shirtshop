3c24f9e7984d631f5dd56fb990bda53eee69e65d
muhia <109690494+Xmuhia@users.noreply.github.com>
2023-11-01 13:18:04 +0300

    Update CameraRig.jsx

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -11,6 +11,19 @@ const CameraRig = ({children}) => {
   const snap = useSnapshot(state);
 
   useFrame((state, delta) => {
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+
+
+    //set the initial position of the model
+    let targetPosition = [-0.4, 0, 2];
+    if(snap.intro){
+      if(isBreakpoint) targetPosition = [0, 0, 2];
+      if(isMobile) targetPosition = [0, 0.2, 2.5];
+    } else {
+      if(isMobile) targetPosition = [0, 0, 2.5]
+      else targetPosition = [0, 0, 2];
+    }
 
   //set the model rotation smoothly
     easing.dampE(
