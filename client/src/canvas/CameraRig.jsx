6a4ad7764ea05d6747e55a42875636c6250ccfb5
muhia <109690494+Xmuhia@users.noreply.github.com>
2023-11-02 23:44:17 +0300

    Fixing Bugs on Camera Rotation

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,46 +1,52 @@
-import React, { Children, useRef } from 'react';
+import React, {useRef} from 'react'
 import { useFrame } from '@react-three/fiber';
 import { easing } from 'maath';
-import { useSnapshot } from 'valtio';
 
 import state from '../store';
+import { useSnapshot } from 'valtio';
 
 
 const CameraRig = ({children}) => {
   const group = useRef();
-  const snap = useSnapshot(state);
+  const snap = useSnapshot;
+
+
 
   useFrame((state, delta) => {
     const isBreakpoint = window.innerWidth <= 1260;
     const isMobile = window.innerWidth <= 600;
 
-
     //set the initial position of the model
     let targetPosition = [-0.4, 0, 2];
-    if(snap.intro){
-      if(isBreakpoint) targetPosition = [0, 0, 2];
-      if(isMobile) targetPosition = [0, 0.2, 2.5];
+    if(snap.intro) {
+        if (isBreakpoint) targetPosition = [0, 0, 2];
+        if(isMobile) targetPosition = [0, 0.2, 2.5]
     } else {
-      if(isMobile) targetPosition = [0, 0, 2.5]
+      if(isMobile) targetPosition = [0, 0.2, 2.5]
       else targetPosition = [0, 0, 2];
     }
 
-  //set the model rotation smoothly
-    easing.dampE(
-      group.current.rotation, [state.pointer.y / 10, -state.pointer.x / 5, 0],
-      0.25,
-      delta
-    )
+    //set model camera position
+    easing.damp3(
+      state.camera.position, 
+      targetPosition, 
+      0.25, 
+      delta)
+
+
+    //set the model rotation smoothly
+  easing.dampE(
+    group.current.rotation,
+    [state.pointer.y / 10, -state.pointer.x / 5, 0],
+    0.25,
+    delta
+  )
   })
 
-
-
-
+  
   return (
-    <group>
-      {children}
-    </group>
+    <group ref={group}>{children}</group>
   )
 }
 
-export default CameraRig
+export default CameraRig;
