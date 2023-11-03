a387279cb9e7b6d3d6738d48e1d116408112e804
muhia <109690494+Xmuhia@users.noreply.github.com>
2023-11-03 23:56:08 +0300

    Adding Full Texture & Logo Decal

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -12,18 +12,44 @@ const Shirt = () => {
     const logoTexture = useTexture(snap.logoDecal)
     const fullTexture = useTexture(snap.fullDecal)
 
+    useFrame((state, delta) => easing.dampC(materials.lambert1.color, 
+      snap.color, 0.25, delta))
+
+      console;
+  const stateString = JSON.stringify(snap);
+    
 
   return (
-    <group>
-        <mesh 
-            castShadow
-            geometry={nodes.T_Shirt_male.geometry}
-            material={materials.lambert1}
-            dispose={null}
-            material-roughness ={1}
-        >
+    <group
+      key={stateString}
+    >
+       <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+       >
+        {snap.isFullTexture && (
+          <Decal
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
 
-        </mesh>
+         {snap.isLogoTexture && (
+          <Decal
+            position={[0, 0.04, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.15}
+            map={logoTexture}
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
+      </mesh>
     </group>
   )
 }
