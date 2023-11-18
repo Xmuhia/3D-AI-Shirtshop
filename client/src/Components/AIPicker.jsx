8184be9a5326568b54405f95b76a389987e74822
muhia <109690494+Xmuhia@users.noreply.github.com>
2023-11-18 21:59:17 +0300

    Add Prompt Functionality AIPicker.jsx

--- client/src/Components/AIPicker.jsx
+++ client/src/Components/AIPicker.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+import CustomButton from './CustomButton';
 
-const AIPicker = () => {
+
+const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit}) => {
   return (
-    <div>AIPicker</div>
+    <div className='aipicker-container'>
+      <textarea
+        placeholder='Ask Ai for help...'
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className='aipicker-textarea'
+        style={{ color: 'white' }}
+      />
+      <div className='flex flex-wrap gap-3'>
+        {generatingImg ? (
+          <CustomButton
+            type='outline'
+            title='AI is Creating...'
+            customStyles='text-xs'
+          />
+        ) : (
+          <>
+            <CustomButton
+              type='outline'
+              title='AI Logo'
+              handleClick={() => handleSubmit('logo')}
+              customStyles='text-xs'
+            />
+
+            <CustomButton
+              type='outline'
+              title='AI Full'
+              handleClick={() => handleSubmit('full')}
+              customStyles='text-xs'
+            />
+          </>
+        )}
+      </div>
+      
+    </div>
   )
 }
 
